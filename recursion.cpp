6d0be628b2b3b6cbb83effe57480c182463b6b55
MCUxKILLER <75075514+MCUxKILLER@users.noreply.github.com>
2023-02-13 22:42:23 +0530

    pick and not pick technique

--- recursion.cpp
+++ recursion.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
+
 using namespace std;
 
 // void swap(int *i,int *j){
@@ -79,6 +81,96 @@ void printF(int idx,vector<int>&ds,int arr[],int n){
 
 }
 
+// print subsequences with sum = k
+void subsum(int idx,vector<int>&ds,int s,int sum,int arr[],int n){
+    if(idx==n){
+        if(s==sum){
+            for(auto i : ds){
+            cout<< i << " ";
+        }
+        cout<<endl;
+        return;
+        }
+        return;
+    }
+    s += arr[idx];
+    ds.push_back(arr[idx]);
+    subsum(idx+1,ds,s,sum,arr,n);
+    s-=arr[idx];
+    ds.pop_back();
+    subsum(idx+1,ds,s,sum,arr,n);
+}
+
+// combination sum - I ( my method)
+
+void comb(int idx,int s,int sum,vector<int>&ds,int arr[],int n){
+    if(s>sum) return;
+    if(idx == n){
+        if(s == sum){
+            for(auto i : ds){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+            return;
+        }
+        return;
+    }
+
+    s+=arr[idx];
+    ds.push_back(arr[idx]);
+    comb(idx,s,sum,ds,arr,n);
+    s-=arr[idx];
+    ds.pop_back();
+    comb(idx+1,s,sum,ds,arr,n);
+}
+
+// combination sum - I by striver
+
+void combstr(int idx,int target,vector<int>&ds,int arr[],int n){
+    if(idx == n){
+        if(target == 0){
+            for(auto i : ds){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+            return;
+        }
+        return;
+    }
+
+    if(arr[idx]<=target){
+    ds.push_back(arr[idx]);
+    combstr(idx,target-arr[idx],ds,arr,n);
+    ds.pop_back();
+    }
+    
+    combstr(idx+1,target,ds,arr,n);
+}
+
+// subset sum - I
+
+void subsetsum(int idx,int sum,int arr[],int n,vector<int>&ds){
+    if(idx==n){
+        ds.push_back(sum);
+        return;
+    }
+    subsetsum(idx+1,sum+arr[idx],arr,n,ds);
+    subsetsum(idx+1,sum,arr,n,ds);
+}
+
+// subset sum - II
+
+void subsetsum2(int idx,vector<int>arr,vector<int>&ds,vector<vector<int>>&ans){
+    ans.push_back(ds);
+    for(int i = idx;i<arr.size();i++){
+        if(i>idx && arr[i]==arr[i-1])
+            continue;
+        ds.push_back(arr[i]);
+        subsetsum2(i+1,arr,ds,ans);
+        ds.pop_back();
+    }
+}
+
 int main(){
     int i;
     // cin>>i;
@@ -90,9 +182,15 @@ int main(){
     // }
     // string s = "madam";
     // cout<<palindrome(0,s);
-
-  int arr[] = {3,1,2};
-  vector<int>ds;
-  printF(0,ds,arr,sizeof(arr)/sizeof(arr[0]));
+    vector<int>ds;
+    vector<vector<int>>ans;
+    vector<int>num = {1,2,2};
+    sort(num.begin(),num.end());
+    subsetsum2(0,num,ds,ans);
+    for (int i = 0; i < ans.size(); i++) {
+        for (auto it = ans[i].begin();it!=ans[i].end(); it++)
+            cout << *it << " ";
+        cout << endl;
+    }
     return 0;
 }
